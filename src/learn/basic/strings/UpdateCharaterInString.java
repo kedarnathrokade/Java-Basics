ab97459210dd4bb6221a7787660ec33868598643
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-07-28 00:16:28 +0530

    program added

---
+++ src/learn/basic/strings/UpdateCharaterInString.java
@@ -0,0 +1,20 @@
+package learn.basic.strings;
+
+public class UpdateCharaterInString {
+	
+	public static void main(String[] args) {
+		
+		String s = "THis is java";
+		
+		int index= 1;
+		
+		char ch = 'h';
+		
+		System.out.println("Original String is : "+s);
+		
+		s= s.substring(0,index)+ ch + s.substring(index+1);
+		
+		System.out.println("After updating element : "+s);
+	}
+
+}
