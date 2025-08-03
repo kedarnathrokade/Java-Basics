572bb995830fa1ea05adfd3f1aa76c975c4dd5fd
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-08-03 20:56:10 +0530

    ++ Occurence of charater is added

---
+++ src/learn/basic/strings/OccurenceOfCharater/OccurrenceOfCharater.java
@@ -0,0 +1,37 @@
+package learn.basic.strings.OccurenceOfCharater;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+// Using the Map
+
+public class OccurrenceOfCharater {
+	
+	public static void main(String[] args) {
+		
+		String s = "Hello World";
+		
+		s = s.replaceAll("\\s+", "");
+		
+		Map<Character, Integer> count = new HashMap<>(); 
+		
+		for(char c: s.toCharArray()) {
+			
+			c = Character.toLowerCase(c);
+			
+			count.put(c, count.getOrDefault(c, 0)+1);
+		}
+		
+		System.out.println("Count of charaters :");
+		for(Map.Entry<Character, Integer> entry : count.entrySet()) {
+			
+			System.out.println(""+entry.getKey()+":"+entry.getValue());
+		}
+		
+		
+		
+		
+	}
+
+}
