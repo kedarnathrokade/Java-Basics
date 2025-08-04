d3a676d84fc9a50733b8b29c74d8a88efe57456e
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-08-04 14:22:15 +0530

    ++ Adding  OccurrenceOfCharacterUsingTreeMap.java program

---
+++ src/learn/basic/strings/OccurenceOfCharater/OccurrenceOfCharacterUsingTreeMap.java
@@ -0,0 +1,27 @@
+package learn.basic.strings.OccurenceOfCharater;
+import java.util.Map;
+import java.util.TreeMap;
+public class OccurrenceOfCharacterUsingTreeMap {
+
+    public static void main(String[] args) {
+        String s = "Java Development". toLowerCase();
+
+        s = s.replaceAll("\\s+","");
+
+        Map<Character, Integer> map = new TreeMap<>();
+        for (char c : s.toCharArray()){
+
+            map.put(c, map.getOrDefault(c, 0)+1);
+
+
+        }
+
+        for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        
+
+    }
+
+}
