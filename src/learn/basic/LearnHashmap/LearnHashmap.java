9bc7b36016ba44a6d5acf9325a11ddbe21ad4cb0
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-08-07 18:10:34 +0530

    Updated Hashmap program

--- src/learn/basic/LearnHashmap/LearnHashmap.java
+++ src/learn/basic/LearnHashmap/LearnHashmap.java
@@ -8,7 +8,7 @@ public class LearnHashmap {
 		
 		HashMap<String,String> m = new HashMap<>();
 		
-		m.put("1","kedar");
+		m.put("1","kedar");		// put() method is used to add the element in hashmap
 		m.put("2","sourabh");
 		m.put("3","ashish");
 		m.put("4","vaibhav");
@@ -16,11 +16,12 @@ public class LearnHashmap {
 		
 		
 		
-		System.out.println(m.get("5"));
-		System.out.println(m.containsKey("4"));
+		System.out.println(m.get("5")); // get() method is used to retrieve the data 
+		System.out.println(m.containsKey("4"));  // containsKey() method will check whether element is exist or not. If exist it returns true else return false
+		System.out.println(m.entrySet());		// entrySet() method is used to retrieve all the elements in hashamap object.
+		System.out.println(m.remove("3"));  	// remove() method is used to remove the element
 		System.out.println(m.entrySet());
-		
-		
+		System.out.println(m.hashCode());
 		
 	}
 
