2e04181530b5e31866de66dd9b2682763402fa82
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-12-09 00:43:38 +0530

    learning null values in hashmap

--- src/learn/LearnHashmap/LearnHashmap.java
+++ src/learn/LearnHashmap/LearnHashmap.java
@@ -8,20 +8,27 @@ public class LearnHashmap {
 		
 		HashMap<String,String> m = new HashMap<>();
 		
-		m.put("1","kedar");		// put() method is used to add the element in hashmap
-		m.put("2","sourabh");
-		m.put("3","ashish");
-		m.put("4","vaibhav");
-		m.put("5","Ram");
+		
+		m.put(null, "asd");
+		m.put("asd", "SS");
+		m.put(null, "a");
+		
+		System.out.println(m);
+		
+//		m.put("1","kedar");		// put() method is used to add the element in hashmap
+//		m.put("2","sourabh");
+//		m.put("3","ashish");
+//		m.put("4","vaibhav");
+//		m.put("5","Ram");
+//		
 		
 		
-		
-		System.out.println(m.get("5")); // get() method is used to retrieve the data 
-		System.out.println(m.containsKey("4"));  // containsKey() method will check whether element is exist or not. If exist it returns true else return false
-		System.out.println(m.entrySet());		// entrySet() method is used to retrieve all the elements in hashamap object.
-		System.out.println(m.remove("3"));  	// remove() method is used to remove the element
-		System.out.println(m.entrySet());
-		System.out.println(m.hashCode());
+//		System.out.println(m.get("5")); // get() method is used to retrieve the data 
+//		System.out.println(m.containsKey("4"));  // containsKey() method will check whether element is exist or not. If exist it returns true else return false
+//		System.out.println(m.entrySet());		// entrySet() method is used to retrieve all the elements in hashamap object.
+//		System.out.println(m.remove("3"));  	// remove() method is used to remove the element
+//		System.out.println(m.entrySet());
+//		System.out.println(m.hashCode());
 		
 		
 	}
