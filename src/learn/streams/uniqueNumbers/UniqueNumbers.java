5bc58023430d47807a2ed10c4d6716aba0595a58
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-12-16 23:49:44 +0530

    UniqueNumbers program using stream

---
+++ src/learn/streams/uniqueNumbers/UniqueNumbers.java
@@ -0,0 +1,15 @@
+package learn.streams.uniqueNumbers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UniqueNumbers {
+	
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,4,1,2,4,5);
+		
+		System.out.println("Unique Elements: "+list.stream().distinct().toList());
+		
+	}
+
+}
