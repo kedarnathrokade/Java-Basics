7ac90980285a047e2187344f982982423df5aa87
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-12-16 23:37:02 +0530

    EvenOdd program added using the streams

---
+++ src/learn/streams/EvenOdd/EvenOdd.java
@@ -0,0 +1,23 @@
+package learn.streams.EvenOdd;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenOdd {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1,2,34,5);
+		
+		System.out.println("Even numbers");
+		
+		list.stream().filter(n -> n%2==0).forEach(System.out::println);
+		
+		System.out.println("Odd numbers");
+		
+		list.stream().filter(n -> n%2 !=0).forEach(System.out::println);
+		
+		
+	}
+
+}
