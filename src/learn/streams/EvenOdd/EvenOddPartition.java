7594370dcc7106ade0ba10012e634a64e68f7d4d
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-12-16 23:44:13 +0530

    EvenOdd program is done using partitioningBy

---
+++ src/learn/streams/EvenOdd/EvenOddPartition.java
@@ -0,0 +1,22 @@
+package learn.streams.EvenOdd;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EvenOddPartition {
+	
+	public static void main(String[] args) {
+		List <Integer> list = Arrays.asList(123,435,213,56,63);
+		
+	Map<Boolean, List<Integer>> result=
+				list.stream().collect(Collectors.partitioningBy(n-> n%2 ==0));
+	
+	System.out.println("Even Numbers: "+result.get(true));
+	
+	System.out.println("Odd Numbers"+result.get(false));
+	
+	}
+
+}
