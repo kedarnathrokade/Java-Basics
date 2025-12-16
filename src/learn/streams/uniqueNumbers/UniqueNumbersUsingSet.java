85774026b9a0d003c1815782e7321791e7a45d8e
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2025-12-16 23:52:47 +0530

    unique elements using set

---
+++ src/learn/streams/uniqueNumbers/UniqueNumbersUsingSet.java
@@ -0,0 +1,18 @@
+package learn.streams.uniqueNumbers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UniqueNumbersUsingSet {
+	
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3,4,2,1);
+		
+		Set<Integer> result = list.stream().collect(Collectors.toSet());
+		
+		System.out.println("Unique Elements : "+result);
+	}
+
+}
