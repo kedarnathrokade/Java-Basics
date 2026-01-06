5d318ae81e4f46597a4ec5d2bb5774690603c08f
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-01-07 00:14:40 +0530

    Thread with anynoumous class

--- src/learn/Threads/LearnThreads.java
+++ src/learn/Threads/LearnThreads.java
@@ -1,39 +1,82 @@
 package learn.Threads;
 
-class Z{
-	
-	
-}
 
-class A implements Runnable{
-	
-	public void run() {
-		
-		for(int i =0; i<=5;i++)
-		
-		System.out.println("Hi");
-		
-	}
-}
+//class A implements Runnable{
+//	
+//	public void run() {
+//		
+//		for(int i =0; i<=5;i++) {
+//		
+//		System.out.println("Hi");
+//		}
+//		
+//		try {
+//			Thread.sleep(500);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
 
 
-class B implements Runnable{
-	
-	public void run() {
-		for(int i =0; i<=5;i++)
-		System.out.println("Hello");
-		
-	}
-	
-}
+//class B implements Runnable{
+//	
+//	public void run() {
+//		for(int i =0; i<=5;i++) {
+//		System.out.println("Hello");  }
+//		
+//		try {
+//			Thread.sleep(500);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//			
+//	}
+//	
+//}
 public class LearnThreads {
 	
 	public static void main(String[] args) {
 		
-		Runnable a = new A();
-		Runnable b = new B();
+		Runnable a =  new Runnable()  {
+			public void run() {
+				
+				for(int i =0; i<=5;i++) {
+				
+				System.out.println("Hi");
+				}
+				
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		};
+		Runnable b = new Runnable() {
+
+			public void run() {
+				for(int i =0; i<=5;i++) {
+				System.out.println("Hello");  }
+				
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+					
+			}
+			
+			
+		};
 		
 		Thread t1 = new Thread(a);
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		Thread t2 = new Thread(b);
 		
 		
