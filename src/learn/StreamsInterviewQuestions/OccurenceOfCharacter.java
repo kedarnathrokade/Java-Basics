1f181474e4bc5ed5d6852761d916ab4fa4150c7f
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-01 23:27:45 +0530

    OccurenceOfCharacter updated

--- src/learn/StreamsInterviewQuestions/OccurenceOfCharacter.java
+++ src/learn/StreamsInterviewQuestions/OccurenceOfCharacter.java
@@ -14,10 +14,16 @@ public class OccurenceOfCharacter {
 	
 			Map<Character, Long> result = str.chars().mapToObj(c -> (char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 			
+			System.out.println("Occurence of Characters: "+result);
+			System.out.println();
+			
+			System.out.println("Only Duplicate Characters: ");
+			
+			
 			result.forEach((ch,count)->{
 				
-				if(count >= 2) {
-					System.out.println(ch+": "+count);
+				if(count >1) {
+					System.out.println(ch+": "+count); // It will print duplicate characters
 				}
 				
 			});
