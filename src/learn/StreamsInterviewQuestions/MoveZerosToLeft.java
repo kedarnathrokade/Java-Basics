668f17885a2d291d0592061a21a49cdce07bbbf9
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-05 00:34:41 +0530

    MoveZerosToLeft Method 2 added

--- src/learn/StreamsInterviewQuestions/MoveZerosToLeft.java
+++ src/learn/StreamsInterviewQuestions/MoveZerosToLeft.java
@@ -1,5 +1,6 @@
 package learn.StreamsInterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +17,17 @@ public class MoveZerosToLeft {
 		
 		List<Integer> result = Stream.concat(list.stream().filter(x -> x==0), list.stream().filter(x -> x!=0))
 				.collect(Collectors.toList());
-		System.out.println(result);
+		System.out.println("Using Stream.concat() method : "+result);
+		
+		// Method 2 : Add zeros one arraylist and add non zeros to another arraylist then add both
+		
+		List<Integer> zeros = list.stream().filter(n -> n==0).collect(Collectors.toList());
+		List<Integer> nonZeros = list.stream().filter(n ->n!=0).collect(Collectors.toList());
+		
+		List<Integer> ans = new ArrayList<>();
+		ans.addAll(zeros);
+		ans.addAll(nonZeros);
+		System.out.println("Using method 2 : "+ans);
 		
 	}
 
