cc742c3180c5727115da6380474b599f6446f274
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-04 23:11:48 +0530

    ProductOfFirstAndLast updated

--- src/learn/StreamsInterviewQuestions/ProductOfFirstAndLast.java
+++ src/learn/StreamsInterviewQuestions/ProductOfFirstAndLast.java
@@ -11,6 +11,8 @@ public class ProductOfFirstAndLast {
 		
 		int[] arr = {5,3,2,7,8,3,5};
 		
+		System.out.println("Using IntStream: ");
+		
 		IntStream.range(0, arr.length/2).map(x -> arr[x]*arr[arr.length-x-1])
 				 .forEach(System.out::println); // Using IntStream
 		
@@ -18,9 +20,12 @@ public class ProductOfFirstAndLast {
 			.map(x -> arr[x]*arr[arr.length-x-1])
 			.toArray();
 		
-		System.out.println("Using array : "+Arrays.toString(result));
+		System.out.println("Using array : \n"+Arrays.toString(result));
 		
+		List<Integer> ans = IntStream.range(0, arr.length/2)
+		.map(x -> arr[x]*arr[arr.length-x-1]).boxed().toList();
 		
+		System.out.println("Using ArrayList : \n"+ans);
 		 
 	}
 
