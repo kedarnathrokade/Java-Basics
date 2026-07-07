60338cca7b73ddb2a8cbec57e355decb7804feae
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-07 18:34:10 +0530

    ReuseOfStreams added

---
+++ src/learn/StreamsInterviewQuestions/ReuseOfStreams.java
@@ -0,0 +1,20 @@
+package learn.StreamsInterviewQuestions;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class ReuseOfStreams {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = List.of(1,2,3,4,5);
+		
+		Supplier<Stream<Integer>> stream = () -> list.stream();
+		stream.get().forEach(System.out::println); // terminal operation
+		
+		long count = stream.get().count(); // terminal operation
+		System.out.println("count: "+count);
+	}
+
+}
