d2a79ebb979c04fad31843c8bcbd450dda5d1648
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-15 13:46:39 +0530

    GroupByCityGetOnlyNameAndDepartment added

---
+++ src/learn/StreamCollectionPrograms/GroupByCityGetOnlyNameAndDepartment.java
@@ -0,0 +1,28 @@
+package learn.StreamCollectionPrograms;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupByCityGetOnlyNameAndDepartment {
+	
+	public static void main(String[] args) {
+		
+List<Employee>  employees = List.of(
+				
+				new Employee(1, "John", "IT", "Pune", 100000),
+				new Employee(2, "Alice", "HR", "Mumbai", 80000),
+				new Employee(3, "Bob", "IT", "Mumbai", 70000),
+				new Employee(4, "Lucy", "Finance", "Pune", 50000)
+				);
+		
+	Map<String, List<String>> result = employees.stream()
+			.collect(Collectors.groupingBy(Employee::getCity, 
+				Collectors.mapping(e -> e.getName() +" - "+ e.getDepartment(), Collectors.toList())));
+	
+	System.out.println(result);
+	
+	
+	}
+
+}
