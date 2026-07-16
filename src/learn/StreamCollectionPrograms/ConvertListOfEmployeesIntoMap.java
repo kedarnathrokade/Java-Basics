82dd49879b7f34a820f20743a614f548f9f65e32
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-16 23:51:24 +0530

    ConvertListOfEmployeesIntoMap with duplicate values and updating old values

--- src/learn/StreamCollectionPrograms/ConvertListOfEmployeesIntoMap.java
+++ src/learn/StreamCollectionPrograms/ConvertListOfEmployeesIntoMap.java
@@ -10,9 +10,18 @@ public class ConvertListOfEmployeesIntoMap {
 		
 			
 		Map<Integer, Employee> ListToMap = Employee.getEmployees().stream()
-				.collect(Collectors.toMap(Employee::getId, e -> e));
+				.collect(Collectors.toMap(Employee::getId, e -> e));  // This is for without duplicate keys
 		
-		System.out.println(ListToMap);
+		// if duplicate keys are present then it will throws : IllegalStateException: Duplicate key 
+		
+		System.out.println("Without duplicates: "+ListToMap);
+		
+		Map<Integer, Employee> ListToMapWithOldDuplicates = Employee.getEmployees().stream()
+				.collect(Collectors.toMap(Employee::getId, e-> e, (oldValue, newValue) -> oldValue)); // This will keep old duplicate values
+		
+		System.out.println("With Duplicates and updating old values: "+ListToMapWithOldDuplicates);
+		
+		
 		
 
 	}
