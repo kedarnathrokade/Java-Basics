d029923e4b6d2baeab50079cfeb6992d1136d0dd
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-17 00:34:03 +0530

    MinimumSalary added

---
+++ src/learn/StreamCollectionPrograms/MinimumSalary.java
@@ -0,0 +1,26 @@
+package learn.StreamCollectionPrograms;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MinimumSalary {
+	
+	public static void main(String[] args) {
+		
+		List<Employee>  employees = List.of(
+				
+				new Employee(1, "John", "IT", "Pune", 100000),
+				new Employee(2, "Alice", "HR", "Mumbai", 80000),
+				new Employee(3, "Bob", "IT", "Mumbai", 70000),
+				new Employee(4, "Lucy", "Finance", "Pune", 50000)
+				);
+		
+		Employee minimumSalary = employees.stream()
+				.min(Comparator.comparing(Employee::getSalary))
+				.orElse(null);
+		
+		System.out.println(minimumSalary);
+		
+	}
+
+}
