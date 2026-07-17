9ffecb90ecdbced43a854b1e13ca0e25ccbbee71
Kedarnath Rokade <61425279+kedarnathrokade@users.noreply.github.com>
2026-07-17 14:45:33 +0530

    C:/Program Files/Git/NthLowestSalary added

---
+++ src/learn/StreamCollectionPrograms/NthLowestSalary.java
@@ -0,0 +1,29 @@
+package learn.StreamCollectionPrograms;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class NthLowestSalary {
+
+	public static void main(String[] args) {
+		
+		List<Employee>  employees = List.of(
+				
+				new Employee(1, "John", "IT", "Pune", 100000),
+				new Employee(2, "Alice", "HR", "Mumbai", 80000),
+				new Employee(3, "Bob", "IT", "Mumbai", 70000),
+				new Employee(4, "Lucy", "Finance", "Pune", 50000)
+				);
+		
+		int n=2;
+		
+		Employee nThLowestSalary = employees.stream()
+				.sorted(Comparator.comparing(Employee::getSalary))
+				.skip(n-1)
+				.findFirst()
+				.orElse(null);
+		
+		System.out.println(nThLowestSalary);
+	}
+	
+}
